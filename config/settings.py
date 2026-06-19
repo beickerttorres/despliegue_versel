4d57d0e9b025465d797dfe65f5bccac89a89eed6
beickert <beickerttapia@gmail.com>
2026-06-18 23:46:41 -0500

    Fix ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -6,8 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='clave-insegura-solo-para-desarrollo')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
-
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'despliegue-versel-2.vercel.app',
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
